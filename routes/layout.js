346cb70674834883f61ea9c46e6b1186e4e9cc52
icerayongsi <icerayongsi@gmail.com>
2022-10-29 10:56:10 +0700

    modified:   routes/layout.js 	modified:   views/dashboard/Injection/Injection_partials/Injection_menu.ejs 	modified:   views/dashboard/Injection/Injection_partials/Injection_script.ejs 	modified:   views/dashboard/Injection/inj1/Injection_inj1.ejs 	modified:   views/main/layout_wellgrow.ejs

--- routes/layout.js
+++ routes/layout.js
@@ -55,7 +55,7 @@ io.on("connection", (socket) => {
       
       message = JSON.parse(message.toString());
       message.forEach(element => {
-        socket.emit('inj-' + element.MC_NameID , {
+        socket.emit('inj-' + element.Name , {
           data : element
         });
       });
@@ -66,7 +66,7 @@ io.on("connection", (socket) => {
       message = JSON.parse(message.toString());
       
       message.forEach(element => {
-        socket.emit('inj-data-' + element.MC_NameID , {
+        socket.emit('inj-data-' + element.Name , {
           data : element
         });
       });
@@ -1368,7 +1368,7 @@ router.get('/BrazingOIL', function (req, res, next) {
 router.get('/Injection', function (req, res, next) {
   // if (!req.session.userid) res.redirect('/login');
 
-  res.render('dashboard/Injection/Injection', { title: title, header: 'Injection',inj : req.query.inj , page: req.query.page });
+  res.render('dashboard/Injection/Injection', { title: title, header: 'Injection',inj : req.query.inj , name : req.query.name , page: req.query.page });
 });
 
 // END BrazingGIC2
